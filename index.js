88dfe5989859f51ece9cd8126e9341ba53076f66
vikram <v4vikram.dev@gmail.com>
2022-11-11 14:58:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const PORT = process.env.PORT || 4000
 
 http.createServer((req,res)=>{
-    res.write(`<h1>Hello vikram</h1>`);
+    res.write(`<h1>Hello vikrams</h1>`);
     res.end();
 }).listen(PORT, ()=>{
     console.log(`Server run on ${PORT}`)
